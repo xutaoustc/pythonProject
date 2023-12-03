9ba855309602230fcf41e20986d3e59cf93defc3
xutao <xutao_ustc@163.com>
2023-12-03 22:30:29 +0800

    refactor, because it is record in note

--- study_04_container.py
+++ study_04_container.py
@@ -1,67 +1,26 @@
-# 非数字
-#   字符串
-#   列表 - 有序可变序列（*在其他语言中通常叫数组）
-#   元组 - 有序不可变序列
-#   集合 - 无序不重复集合
-#   字典
 
-# 所有非数字类型变量都有以下特点：
-#   都是一个序列sequence，可以理解为容器
-#   取值[]
-#   遍历for in
-#   计算长度、最大/最小值、比较、删除
-#   链接+和重复*
-#   切片
 
-def list_test():
-    """
-    增加： insert
-          append
-          extend
-    删除： del
-          remove
-          pop
-          pop()
-          clear
-    取值，取索引：
-          下标
-          index
-    统计： len
-          count
-    排序： sort
-          reverse
-    :return:
-    """
-    # 列表中存储的类型可以是不同的数据类型
-    my_list = ["1", "2", "3"]
-    my_list_1 = ["1", 2, True]
 
-    # 下标访问
-    element = my_list[0]
-    element = my_list[-1]
-    # 修改
-    my_list[1] = "22"
 
-    # 获取元素的下标，如果找不到则报错
-    index = my_list.index("3")
-    print(index)
+# def dict_test():
+#     dict = {"xu": 1}
+#     # 没有找到对应key会报错
+#     val = dict["xu"]
+#
+#     # 没有找到对应key会报错
+#     dict.pop("xu")
+#
+#     for k in dict:
 
-    # insert, append, extend
-    my_list.insert(1, "ins")
-    my_list.append("4")
-    my_list.extend(["ao", "li", "gei"])
-    print(my_list)
 
-    # delete, pop, remove, clear
-    del my_list[1]
-    pop_element = my_list.pop(1)
-    my_list.remove("ao")  # 删除第一个匹配的项
-    my_list.clear()
+def str_test():
+    name = '黑马程序员'
+    name = "黑马程序员"
+    name = """黑马程序员"""   # 用变量接收是字符串，单独放置是注释
 
-    # 统计某元素在列表内的数量
-    my_list.count("li")
+    # +号字符串拼接： 可以完成字符串和字符串之间的拼接； 但无法完成字符串和其他类型的拼接，name = "黑马程序员" + 1.0
+    # 字符串格式化拼接：字符串和其他类型的拼接要使用字符串格式化
+    i = 10
+    name = f"黑马程序员{i}"
 
-    # 统计列表元素数量
-    print(len(my_list))
 
-list_test()
